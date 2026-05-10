3f210b10a600385f3d586c1b2e733ed2d805a13b
Gon-Silva <goncalo.silva.t0123533@edu.atec.pt>
2026-05-10 20:44:54 +0100

    Criado o ficheiro com os caminhos para os ficheiros json

---
+++ Code/config.py
@@ -0,0 +1,8 @@
+# File with all path
+STATEMENT_PATH = "DataBase/statement.json"
+MOVIES_PATH = "DataBase/Movies/movies.json"
+CLIENTS_PATH = "DataBase/Client/clients.json"
+EMPLOYEES_PATH = "DataBase/Employees/employees.json"
+RENTALS_PATH = "DataBase/Rentals/rentals.json"
+PLANS_PATH = "DataBase/subscription_plans.json"
+COMMENTS_PATH = "DataBase/Comment/"
