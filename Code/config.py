9dcd479e2568d22833809efc36bbd4d733439bd3
Gon-Silva <goncalo.silva.t0123533@edu.atec.pt>
2026-05-13 14:21:49 +0100

    Alteração do caminho do CLIENTS_PATH

--- Code/config.py
+++ Code/config.py
@@ -1,7 +1,7 @@
 # File with all path
 STATEMENT_PATH = "DataBase/statement.json"
 MOVIES_PATH = "DataBase/Movies/movies.json"
-CLIENTS_PATH = "DataBase/Client/clients.json"
+CLIENTS_PATH = "DataBase/Clients/clients.json"
 EMPLOYEES_PATH = "DataBase/Employees/employees.json"
 RENTALS_PATH = "DataBase/Rentals/rentals.json"
 PLANS_PATH = "DataBase/subscription_plans.json"
