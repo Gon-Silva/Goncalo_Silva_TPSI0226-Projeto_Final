93a6b0bf1ef8d536330510d64025c933a5ec56b1
Gon-Silva <goncalo.silva.t0123533@edu.atec.pt>
2026-05-14 15:11:59 +0100

    Adição do header do new_account

--- Code/NewAccount/new_account.py
+++ Code/NewAccount/new_account.py
@@ -6,7 +6,7 @@ from GenericUtlis.errors import catch_number_error
 from GenericUtlis.files import read_json
 from GenericUtlis.terminal import cls, press_to_continue
 from Headers.headers import header_new_account
-from NewAccount.new_model_account import NewAccount
+from NewAccount.model_account import NewAccount
 from NewAccount.utils_new_account import save_account
 
 
@@ -28,6 +28,8 @@ def create_account():
 
     while number <= 6:
         cls()
+
+        print(header_new_account)
 
         match number:
             case 1:
