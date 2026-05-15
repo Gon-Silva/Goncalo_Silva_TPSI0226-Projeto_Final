ff20627a4247e4eb160247fe1af3d20e206e4c1e
Gon-Silva <goncalo.silva.t0123533@edu.atec.pt>
2026-05-15 16:04:23 +0100

    Criações de heranças para a client e employee

--- Code/NewAccount/model_account.py
+++ Code/NewAccount/model_account.py
@@ -1,10 +1,12 @@
-from GenericUtlis.terminal import press_to_continue
 from NewAccount.utils_new_account import (
     check_email,
+    check_nif,
+    check_phone,
     validate_age,
     validate_email,
     validate_len_password,
     validate_name,
+    validate_nif,
     validate_phone,
 )
 
@@ -17,20 +19,20 @@ class NewAccount:
         last_name: str,
         age: int,
         phone: str,
+        nif: str,
         email: str,
         password: str,
         is_active: bool,
-        subscription_plan: str,
     ) -> None:
         self.__id = id
         self.__first_name = first_name
         self.__last_name = last_name
         self.__age = age
         self.__phone = phone
+        self.__nif = nif
         self.__email = email
         self.__password = password
         self.__is_active = is_active
-        self.__subscription_plan = subscription_plan
 
     # Getters for private attributes
     @property
@@ -59,6 +61,11 @@ class NewAccount:
         return self.__phone
 
     @property
+    def nif(self) -> str:
+        """Returns NIF account"""
+        return self.__nif
+
+    @property
     def email(self) -> str:
         """Returns account email"""
         return self.__email
@@ -73,11 +80,6 @@ class NewAccount:
         """Returns account active"""
         return self.__is_active
 
-    @property
-    def subscription_plan(self) -> str:
-        """Returns account subscription plan"""
-        return self.__subscription_plan
-
     # Setters for private attributes
     @id.setter
     def id(self, id: int) -> None:
@@ -89,7 +91,6 @@ class NewAccount:
         """Set first name"""
         if not validate_name(first_name):
             raise ValueError("Invalid first name size")
-            return
 
         self.__first_name = first_name
 
@@ -98,7 +99,6 @@ class NewAccount:
         """Set last name"""
         if not validate_name(last_name):
             raise ValueError("Invalid last name size")
-            return
 
         self.__last_name = last_name
 
@@ -107,7 +107,6 @@ class NewAccount:
         """Set age"""
         if not validate_age(age):
             raise ValueError("Invalid age")
-            return
 
         self.__age = age
 
@@ -116,20 +115,31 @@ class NewAccount:
         """Set phone"""
         if not validate_phone(phone):
             raise ValueError("Invalid phone format")
-            return
+
+        if not check_phone(phone):
+            raise ValueError("This phone already exists")
 
         self.__phone = phone
+
+    @nif.setter
+    def nif(self, nif: str) -> None:
+        """Set NIF"""
+        if not validate_nif(nif):
+            raise ValueError("Invalid NIF formant")
+
+        if not check_nif(nif):
+            raise ValueError("This nif already exists")
+
+        self.__nif = nif
 
     @email.setter
     def email(self, email: str) -> None:
         """Set email"""
         if not validate_email(email):
             raise ValueError("Invalid email format")
-            return
 
         if not check_email(email):
             raise ValueError("This email address already exists")
-            return
 
         self.__email = email
 
@@ -138,7 +148,6 @@ class NewAccount:
         """Set password"""
         if not validate_len_password(password):
             raise ValueError("Invalid passowrd size")
-            return
 
         self.__password = password
 
@@ -146,11 +155,6 @@ class NewAccount:
     def is_active(self, is_active: bool) -> None:
         """Set is active"""
         self.__is_active = is_active
-
-    @subscription_plan.setter
-    def subscription_plan(self, subcription_plan: str) -> None:
-        """Set subscription plan"""
-        self.__subscription_plan = subcription_plan
 
     # String representation for debugging/logging
     def __repr__(self) -> str:
@@ -160,10 +164,10 @@ class NewAccount:
             f"last name -> '{self.__last_name}', "
             f"age -> '{self.__age}', "
             f"phone -> '{self.__phone}', "
+            f"nif -> '{self.__nif}', "
             f"email -> '{self.__email}', "
             f"password -> '{self.__password}', "
-            f"subscription plan -> '{self.__subscription_plan}', "
-            f"is active -> '{self.__is_active}')"
+            f"is active -> '{self.__is_active}'), "
         )
 
     # String for the user (Need improvement)
@@ -173,6 +177,7 @@ class NewAccount:
             f" > Last Name - {self.__last_name}\n"
             f" > Age - {self.__age}\n"
             f" > Phone - {self.__phone}\n"
+            f" > NIF -> {self.__nif}\n"
             f" > Email - {self.__email}\n"
             f" > Password - {self.__password}"
         )
@@ -187,8 +192,109 @@ class NewAccount:
             },
             "age": self.__age,
             "phone": self.__phone,
+            "nif": self.__nif,
             "email": self.__email,
             "password": self.__password,
             "is_active": self.__is_active,
-            "subscription_plan": self.__subscription_plan,
         }
+
+
+class NewClient(NewAccount):
+    def __init__(
+        self,
+        id: int,
+        first_name: str,
+        last_name: str,
+        age: int,
+        phone: str,
+        nif: str,
+        email: str,
+        password: str,
+        is_active: bool,
+        subscription_plan: str,
+    ) -> None:
+        super().__init__(
+            id,
+            first_name,
+            last_name,
+            age,
+            phone,
+            nif,
+            email,
+            password,
+            is_active,
+        )
+        self.__subscription_plan = subscription_plan
+
+    # Getters for private attributes
+    @property
+    def subscription_plan(self) -> str:
+        """Returns account subscription plan"""
+        return self.__subscription_plan
+
+    # Setters for private attributes
+    @subscription_plan.setter
+    def subscription_plan(self, subcription_plan: str) -> None:
+        """Set subscription plan"""
+        self.__subscription_plan = subcription_plan
+
+    # String representation for debugging/logging
+    def __repr__(self) -> str:
+        return f"{super().__repr__()}\n > Subscription Plan -> '{self.__subscription_plan}'"
+
+    # String for the user (Need improvement)
+    def __str__(self) -> str:
+        return super().__str__()
+
+    # Convert the class to dict
+    def to_dict(self) -> dict:
+        base = super().to_dict()
+        base["subscription_plan"] = self.__subscription_plan
+        return base
+
+
+class NewEmployee(NewAccount):
+    def __init__(
+        self,
+        id: int,
+        first_name: str,
+        last_name: str,
+        age: int,
+        phone: str,
+        email: str,
+        password: str,
+        is_active: bool,
+        nif: str,
+        role: str,
+    ) -> None:
+        super().__init__(
+            id, first_name, last_name, age, phone, nif, email, password, is_active
+        )
+        self.__role = role
+
+    # Getters for private attributes
+
+    @property
+    def role(self) -> str:
+        """Returns role account"""
+        return self.__role
+
+    # Setters for private attributes
+    @role.setter
+    def role(self, role: str) -> None:
+        """Set role"""
+        self.__role = role
+
+    # String representation for debugging/logging
+    def __repr__(self) -> str:
+        return f"{super().__repr__()}\nrole -> '{self.__role}' ,"
+
+    # String for the user (Need improvement)
+    def __str__(self) -> str:
+        return f"{super().__str__()}\n > Role -> '{self.__role}'"
+
+    # Convert the class to dict
+    def to_dict(self) -> dict:
+        base = super().to_dict()
+        base["role"] = self.__role
+        return base
