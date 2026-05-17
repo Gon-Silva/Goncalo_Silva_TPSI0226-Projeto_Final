3a68dc1489626e981f94979dab3ab05e6fb55b65
Gon-Silva <goncalo.silva.t0123533@edu.atec.pt>
2026-05-17 04:55:22 +0100

    Mudanças dos importes da criação da conta na parte do cliente

--- Code/main.py
+++ Code/main.py
@@ -2,7 +2,7 @@ from GenericUtlis.errors import catch_number_error
 from GenericUtlis.terminal import cls, press_to_continue
 from Headers.headers import header_name_store
 from Login.login import login
-from NewAccount.new_account import new_account
+from NewAccount.new_account import new_account_client
 from Setup.setup import setup
 
 
@@ -37,7 +37,7 @@ def main():
                 login()
                 pass
             case 2:
-                new_account()
+                new_account_client()
                 pass
             case 3:
                 print("\nThank you for using\n")
